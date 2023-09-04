21b0432c680b90933e3440e91315658176c9b4f9
guilhermepfraga <144057954+guilhermepfraga@users.noreply.github.com>
2023-09-04 15:52:52 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var setaDireita = window.document.getElementById("seta-direita")
+var Leonardo = window.document.getElementById("Leonardo")
+var Samatha = window.document.getElementById("Samantha")
+var Bruna = window.document.getElementById("Bruna")
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+
+
+function RolarParaDireita() {
+    Bruna.style = "display:none"
+    Samantha.style = "display:flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = "display:flex"
+}
+
+function RolarParaEsquerda() {
+    Bruna.style = "display:flex"
+    Samantha.style = "display:none"
+    setaDireita.style = "display:flex"
+    setaEsquerda.style = "display:none"
+}
